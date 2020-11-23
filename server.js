68fe52a7b90cbcb63f2d7b9658d5ef6f0bdaf747
Lyne Malele <lynemalele@gmail.com>
2020-11-23 00:08:23 -0500

    Code inquirer template

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
+var inquirer = require("inquirer");
 var mysql = require("mysql");
-var inquirer = require('inquirer');
-inquirer
-  .prompt([
-    /* Pass your questions in here */
-  ])
-  .then(answers => {
-    // Use user feedback for... whatever!!
-  })
-  .catch(error => {
-    if(error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "passwordEmployee",
+    database: "employee_trackerDB"
   });
+
