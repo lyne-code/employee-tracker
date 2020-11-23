c7f1a8c2fdc4565ef66d55b51d06cbc228eb7c41
Lyne Malele <lynemalele@gmail.com>
2020-11-23 00:10:08 -0500

    Code initial prompt

--- server.js
+++ server.js
@@ -15,3 +15,34 @@ var connection = mysql.createConnection({
     database: "employee_trackerDB"
   });
 
+
+// Connection ID 
+connection.connect(function(err) {
+    if (err) throw err
+    // run the start function after the connection is made to prompt the user
+    startPrompt();
+});
+
+// Initial Prompt 
+function start () {
+inquirer
+  .prompt([
+      
+    {
+    type: "list",
+    message: "Hey! What would you like to do?",
+    name: "choice",
+    choices: 
+    [
+        "View All Employees?", 
+        "View All Employee's By Roles?",
+        "View all Emplyees By Deparments", 
+        "Update Employee",
+        "Add Employee?",
+        "Add Role?",
+        "Add Department?"
+    ]
+
+    }
+
+ ])
